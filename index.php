e855c2738abc4bbc8ec6a9eb2419d6a55d571f8f
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-18 08:31:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
-$url = parse_url(getenv("mysql://b5e5dcb1e803f4:a90bd23e@us-cdbr-east-02.cleardb.com/heroku_de162b651ed5bf0?reconnect=true"));
+	$url = parse_url(getenv("mysql://b5e5dcb1e803f4:a90bd23e@us-cdbr-east-02.cleardb.com/heroku_de162b651ed5bf0?reconnect=true"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+	$conn = new mysqli($server, $username, $password, $db);
 
-print_r($conn);
+	print_r($conn);
 
-print_r($url);
+	print_r($url);
 
 
 
-try{
+	try{
 		$dbhCG = new PDO ('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_de162b651ed5bf0', 'b5e5dcb1e803f4', 'a90bd23e');
 		$dbhCG -> query ('SET NAMES utf8');
 		$dbhCG -> query ('SET CHARACTER_SET utf8_unicode_ci');
@@ -22,4 +22,7 @@ try{
 		print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
 		die();
 	}
+		
+		
+	print_r($dbhCG);
 ?>
