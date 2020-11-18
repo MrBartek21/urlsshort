babd2d0824924a5cf21d9a0b306e92247e81eabe
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-18 10:48:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 //const path = require('path');
-//const mysql = require('mysql');
+const mysql = require('mysql');
 const PORT = process.env.PORT || 5000;
 const app = express();
 //app.use(express.logger());
