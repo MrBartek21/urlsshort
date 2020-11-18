f1568d01565bb83ad77ea9846da9c1bdce08242e
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-18 08:47:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 console.log("test index.js");
 
-express()
+/*express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
