8d7a0949f62e555f8084abec308dfa7b6b2b7c72
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-18 08:21:55 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$url = parse_url(getenv("mysql://b5e5dcb1e803f4:a90bd23e@us-cdbr-east-02.cleardb.com/heroku_de162b651ed5bf0?reconnect=true"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+print_r($conn);
+
+print_r($url);
+?>
