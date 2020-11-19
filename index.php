d3fd465970222808749d47f70690ff3ee0255bac
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-19 04:07:54 +0100

    bootstrap

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
         <meta name="description" content="<?php echo $Description;?>"/>
 		
 		<!-- Bootstrap core CSS -->
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
+		<link href="Vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		
 		<!-- Custom styles -->
 		<link href="CSS/Main.css" rel="stylesheet">
@@ -143,8 +143,8 @@
 		</footer>
 		
 		<!-- Bootstrap core JavaScript -->
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-t6I8D5dJmMXjCsRLhSzCltuhNZg6P10kE0m0nAncLUjH6GeYLhRU1zfLoW3QNQDF" crossorigin="anonymous"></script>
+		<script src="Vendor/jquery/jquery.min.js"></script>
+		<script src="Vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 		<script>
 			$(document).ready(function() {
 				// Initialize the tooltip.
