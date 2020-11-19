e88aaea20dfbce9878c8d6f60aba191aa856301d
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-19 02:06:26 +0100

    update

---
+++ Resources/Connect.php
@@ -0,0 +1,9 @@
+<?php
+    $Host = "us-cdbr-east-02.cleardb.com";
+	$DB_User = "b5e5dcb1e803f4";
+	$DB_Password = "a90bd23e";
+	$DB_Name = "heroku_de162b651ed5bf0";
+
+	$Connect = @new mysqli($Host, $DB_User, $DB_Password, $DB_Name_CG);
+    $Connect->set_charset("utf-8");
+?>
---
+++ Resources/Settings.php
@@ -0,0 +1,15 @@
+<?php
+    $Author = "MrBartek21";
+	$Keywords = "url, short, shortener, urls, friendly, link";
+    $Description = "Url shortener";
+    
+	$Title_Index = "Url shortener";
+    $Link_Index = "index.php";
+    
+    $MainPage = "Main page";
+    
+	$Link_Footer = "https://cloudpanel.sytes.net";
+    $Title_Footer = "CloudPanel";
+    
+
+?>
