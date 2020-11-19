961aefb90d70c08680f4edfc808193db2713247d
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-19 03:56:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 
 		$Name = CheckName($Connect);
 		$ShortName = GenerateCode($Connect);
+		echo $ShortName;
 
 		//$Connect->query("INSERT INTO urls (Name, Link, ShortName) VALUES ('$Name', '$url', '$ShortName')");
 
@@ -25,7 +26,7 @@
 		$Link = $Link.'/?url='.$ShortName;
 
 		$GeneratedLink = '<hr /><p class="card-text">The generated link is: <a href="'.$Link.'" class="">'.$Link.'</a></p>';
-		$GeneratedLink = '<hr /><a class="btn btn-success btn-block btn-large" href="'.$Link.'" role="button">'.$Link.'</a>';
+		$GeneratedLink = '<hr /><a class="btn btn-success btn-block btn-large" href="'.$Link.'" role="button" id="copy-input">'.$Link.'</a>';
 		//unset($_POST);
 	}
 
@@ -109,6 +110,18 @@
 										<button type="submit" class="btn btn-primary btn-block">Generate</button>
 									</form>
 
+									<form>
+										<div class="input-group">
+											<input type="text" class="form-control" value="/path/to/foo/bar" placeholder="Some path" id="copy-input">
+											<span class="input-group-btn">
+												<button class="btn btn-default" type="button" id="copy-button" data-toggle="tooltip" data-placement="button" title="Copy to Clipboard">Copy</button>
+											</span>
+										</div>
+									</form>
+
+
+									
+
 									<?php
 										echo $GeneratedLink;
 										echo $Error;
@@ -135,5 +148,38 @@
 		<!-- Bootstrap core JavaScript -->
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-t6I8D5dJmMXjCsRLhSzCltuhNZg6P10kE0m0nAncLUjH6GeYLhRU1zfLoW3QNQDF" crossorigin="anonymous"></script>
+		<script>
+			$(document).ready(function() {
+				// Initialize the tooltip.
+				$('#copy-button').tooltip();
+
+				// When the copy button is clicked, select the value of the text box, attempt
+				// to execute the copy command, and trigger event to update tooltip message
+				// to indicate whether the text was successfully copied.
+				$('#copy-button').bind('click', function() {
+					var input = document.querySelector('#copy-input');
+					input.setSelectionRange(0, input.value.length + 1);
+					try {
+					var success = document.execCommand('copy');
+					if (success) {
+						$('#copy-button').trigger('copied', ['Copied!']);
+					} else {
+						$('#copy-button').trigger('copied', ['Copy with Ctrl-c']);
+					}
+					} catch (err) {
+					$('#copy-button').trigger('copied', ['Copy with Ctrl-c']);
+					}
+				});
+
+				// Handler for updating the tooltip message.
+				$('#copy-button').bind('copied', function(event, message) {
+					$(this).attr('title', message)
+						.tooltip('fixTitle')
+						.tooltip('show')
+						.attr('title', "Copy to Clipboard")
+						.tooltip('fixTitle');
+				});
+			});
+		</script>
 	</body>
 </html>
